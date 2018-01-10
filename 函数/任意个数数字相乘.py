d988482dd931b9ad9ef5a466e8dc28b7927e686f
BenBoErBa-ZhangShiBo <610079410@qq.com>
2018-01-11 00:12:32 +0800

    任意数字相加/相乘

--- 函数/任意个数数字相乘.py
+++ 函数/任意个数数字相乘.py
@@ -1,8 +1,46 @@
 def calc(numbers):
-    sum = 0
+    sum = 1
+    numbers.pop(0)
     for n in numbers:
-        sum = sum + n * n
+        sum = sum * n
     return sum
 
-sum=calc([1,2,3,4,5,6,7])
-print(sum)
+
+def add(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n
+    return sum
+
+
+summer = [0, ]
+a = 0
+while a != -1:
+    print('输入选择运算的编号，输入-1为退出')
+    print('选项1：累加')
+    print('选项2：累乘')
+    a = int(input())
+    if a == -1:
+        exit()
+    if a == 1:
+        b = 0
+        while b != -1:
+            print('输入需要相加的数字，以输入-1为终止')
+            b = int(input())
+            if b != -1:
+                summer.append(b)
+            if b == -1:
+                sum = add(summer)
+                print(sum)
+                exit()
+    elif a == 2:
+        c = 0
+        while c != -1:
+            print('输入需要相乘的数字，以输入-1为终止')
+            c = int(input())
+            if c != -1:
+                summer.append(c)
+            if c == -1:
+                sum = calc(summer)
+                print(sum)
+                exit()
