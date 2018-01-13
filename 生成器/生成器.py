67891504e4bc2228ef4993bcbf034319b51508b5
BenBoErBa-ZhangShiBo <610079410@qq.com>
2018-01-13 18:30:15 +0800

    生成器

---
+++ 生成器/生成器.py
@@ -0,0 +1,14 @@
+def tric():
+    L = [1]
+    while True:
+        yield L
+        L.append(0)
+        L = [L[i - 1] + L[i] for i in range(len(L))]
+
+
+n = 0
+for t in tric():
+    print(t)
+    n += 1
+    if n == 10:
+        break
