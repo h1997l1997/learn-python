6d8ab31b0a9e74bd3c087b876db083153cc5b1ca
BenBoErBa-ZhangShiBo <610079410@qq.com>
2018-01-23 22:05:41 +0800

    web（未完工）

---
+++ 爬虫/test.py
@@ -0,0 +1,31 @@
+from tornado import web, ioloop, httpserver
+
+#页面
+#首页
+class Main(web.RequestHandler):
+    def get(self, *args, **kwargs):
+        #self.write('wellcome')
+        self.render('index.html')
+
+#登录
+class Login(web.RequestHandler):
+    def get(self,*args,**kwargs):
+        self.render('login.html')
+    def post(self,*args,**kwargs):
+        username=self.get_argument('username')
+        password=self.get_argument('password')
+#设置
+setting={
+    'template_path':'templates' #和子文件夹名一样
+}
+# 路由 分机
+application = web.Application([
+    (r"/", Main),
+    (r"/login",Login),
+]**setting)
+
+# socket 服务
+if __name__ == '__main__':
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(8080)
+    ioloop.IOLoop.current().start()
